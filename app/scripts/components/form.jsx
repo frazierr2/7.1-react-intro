94073bf216f78a42ce405a77e8c7546311f77f41
Ryan Frazier <frazierr2@gmail.com>
2016-10-24 18:40:37 -0400

    Banner and Form opening

--- app/scripts/components/form.jsx
+++ app/scripts/components/form.jsx
@@ -1,2 +1,52 @@
 var _ = require('underscore');
 var React = require('react');
+
+
+var ImageImportForm = React.createClass({
+  componentWillMount: function(){
+    this.showForm = false;
+  },
+  handleClick: function(e){
+    e.preventDefault();
+
+    this.showForm = !this.showForm;
+    this.forceUpdate();
+  },
+  render: function(){
+    var display;
+    if(this.showForm){
+      display =(
+        <div className="container">
+          <div className="row well">
+            <div className="form-group row">
+              <div className="col-xs-12">
+              <input className="form-control" type="url" value="" placeholder="Image URL" id="url-input"/>
+              </div>
+            </div>
+            <div className="form-group row">
+              <div className="col-xs-12">
+              <textarea className="form-control" id="textArea" placeholder="Image Caption" rows="3"></textarea>
+              </div>
+            </div>
+            <div className="text-right">
+            <button className="btn">CANCEL</button>
+             <button type="submit" className="btn btn-success">ADD IMAGE</button>
+             </div>
+          </div>
+        </div>
+      )
+    }
+    return(
+      <div>
+        <div className="well banner ">
+          <i className="fa fa-plus-circle add-btn"onClick={this.handleClick} aria-hidden="true"></i>
+        </div>
+        {display}
+      </div>
+    );
+  }
+});
+
+module.exports = {
+  ImageImportForm: ImageImportForm
+}
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -5,11 +5,23 @@ var ReactDOM = require('react-dom');
 
 //Local Imports
 var models = require('./models/image');
-var form = require('./components/form.jsx');
+var form = require('./components/form.jsx').ImageImportForm;
 var listing = require('./components/listing.jsx');
 
 var AppRouter = Backbone.Router.extend({
+routes: {
+  '': 'index'
+},
+initialize: function(){
+  var photos = this.photos = new models.imageCollection();
+},
+index: function(){
 
+  ReactDOM.render(
+    React.createElement(form, {collection: this.photos}),
+    document.getElementById('app')
+  );
+}
 });
 var router = new AppRouter();
 module.exports = router;
